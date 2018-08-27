761a410dc225a5971b27a492cd96de2f7c226e75
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-27 21:26:37 +0530

    swap

---
+++ cspp2-practice/m1/swap.java
@@ -0,0 +1,15 @@
+public class swap
+{
+	public static void main(String[] args)
+	{
+		int a = 10;
+		int b = 20;
+		//int temp = a;
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+
+	}
+}
