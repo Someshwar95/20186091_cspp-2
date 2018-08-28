2e291d0e7c5178bb2c17d1aa4914388201b8caaf
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-28 17:32:57 +0530

    big

---
+++ cspp2-practice/m2/bigger_equal_smaller.java
@@ -0,0 +1,16 @@
+public class bigger_equal_smaller
+{
+	public static void main(String[] args)
+	{
+		int varA = 20;
+		int varB = 30;
+		if (varA > varB)
+			System.out.println("Bigger");
+		else if (varA == varB)
+			System.out.println("Equal");
+		else if (varA < varB)
+			System.out.println("Smaller");
+
+
+	}
+}
