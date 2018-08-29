5a8bd21dbcc0a95f02fbc2aedabb7319f466423c
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-29 11:11:49 +0530

    while

---
+++ cspp2-practice/m3/ConverttoWhileLoop.java
@@ -0,0 +1,13 @@
+public class ConverttoWhileLoop
+{
+	public static void main(String[] args)
+	{
+		int i=2;
+		while(i<11) {
+			System.out.println(i);
+			i=i+2;
+
+		}
+		System.out.println("GoodBye!");
+	}
+}
