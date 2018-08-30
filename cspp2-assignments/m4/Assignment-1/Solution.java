2b786f950b50236837651091ba5b83e99784b42c
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-30 19:56:42 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -10,7 +10,7 @@ public class Solution
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
