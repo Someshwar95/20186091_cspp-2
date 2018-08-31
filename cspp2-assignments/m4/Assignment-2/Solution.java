4f6b0c10da713868e93dbcb37c39ae62447c34b5
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-31 18:36:59 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -35,7 +35,7 @@ public class Solution {
                 }
                 System.out.println();
             }   
-        } if(m==m1 && n==n1) {
+        } else if(m==m1 && n==n1) {
             System.out.println("not possible");
         }
     }
