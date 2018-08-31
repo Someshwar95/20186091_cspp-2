427fcf98aa7955ab329ba68d95edd08bad2b7127
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-31 17:56:02 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -7,33 +7,33 @@ public class Solution {
 
 		int m = sc.nextInt();
 		int n = sc.nextInt();
-		sc.nextLine();
+		
 		int[][] a = new int[m][n];
+        for (int i = 0; i < m ; i++ ) {
+			for (int j = 0; j < n ; j++ ) {
+				a[i][j] = sc.nextInt();
+			}
+		}
 		int m1 = sc.nextInt();
 		int n1 = sc.nextInt();
 		sc.nextLine();
 		int[][] b = new int[m1][n1];
 		int[][] c = new int[3][3];
-        for (int i = 0; i < m ; i++ ) {
-			for (int j = 0; j < n ; j++ ) {
-				a[i][j] = sc.nextInt();
-			}
-			// sc.nextLine();
-		}
 
 		for (int k = 0; k < m1; k++) {
 			for (int l = 0; l < n1; l++) {
 				b[k][l] = sc.nextInt();
 			}
-			// sc.nextLine();	
 		}
 		for (int i = 0;i < m;i++ ) {
 			for (int j = 0;j < n;j++ ) {
-				c[i][j] = a[i][j] + b[i][j];
+				System.out.print(c[i][j]);
 				
 			}
+			System.out.println();
 			
 		}
+
 	}
 }
 	
