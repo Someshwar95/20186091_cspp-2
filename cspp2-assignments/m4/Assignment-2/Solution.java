a770ae5319bd0b36431743ffed8a5bf009f6ae2e
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-31 18:06:14 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,43 +1,43 @@
 import java.util.*;
 public class Solution {
-	public static Scanner sc = new Scanner(System.in);
-	/* Fill the main function to print resultant of addition of matrices*/
-	
-	public static void main(String[] args) {
+    public static Scanner sc = new Scanner(System.in);
+    /* Fill the main function to print resultant of addition of matrices*/
+    
+    public static void main(String[] args) {
 
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		
-		int[][] a = new int[m][n];
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        
+        int[][] a = new int[m][n];
         for (int i = 0; i < m ; i++ ) {
-			for (int j = 0; j < n ; j++ ) {
-				a[i][j] = sc.nextInt();
-			}
-		}
-		int m1 = sc.nextInt();
-		int n1 = sc.nextInt();
-		sc.nextLine();
-		int[][] b = new int[m1][n1];
-		int[][] c = new int[3][3];
+            for (int j = 0; j < n ; j++ ) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        int m1 = sc.nextInt();
+        int n1 = sc.nextInt();
+        sc.nextLine();
+        int[][] b = new int[m1][n1];
+        int[][] c = new int[3][3];
 
-		for (int k = 0; k < m1; k++) {
-			for (int l = 0; l < n1; l++) {
-				b[k][l] = sc.nextInt();
-			}
-		}
-		for (int i = 0;i < m;i++ ) {
-			for (int j = 0;j < n;j++ ) {
-				c[i][j] = a[i][j] + b[i][j];
-				System.out.print(c[i][j]);
-				if(j < n - 1){
-					System.out.print(" ");
-				}
-				
-			}
-			System.out.println();
-			
-		}
+        for (int k = 0; k < m1; k++) {
+            for (int l = 0; l < n1; l++) {
+                b[k][l] = sc.nextInt();
+            }
+        }
+        for (int i = 0;i < m;i++ ) {
+            for (int j = 0;j < n;j++ ) {
+                c[i][j] = a[i][j] + b[i][j];
+                System.out.print(c[i][j]);
+                if(j < n - 1){
+                    System.out.print(" ");
+                }else
+                System.out.println("not possible");
+                
+            }
+            System.out.println();
+            
+        }
 
-	}
+    }
 }
-	
