1cf9ac726d4907e50498f62c3470a5fd4f7ede71
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-31 14:02:44 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
 		for (int i=0;i<3;i++) {
 			for (int j=0;j<3;j++) {
 				c[i][j]=a[i][j]+b[i][j];
-		System.out.println(c[i][j]);
+		System.out.println(c[i][j]+"");
 			}
 		}
 		
