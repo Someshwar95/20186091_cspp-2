7f1fe2a2958b0c3afccd1185ce72a0eda137cb89
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-31 16:02:27 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -9,7 +9,7 @@ public class Solution
     */
     static String reverseString(String s) {
         String str = "";
-        for (int i = s.length()-1;i>=0;i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             char reverse = s.charAt(i);
             str = str + reverse;
         }
