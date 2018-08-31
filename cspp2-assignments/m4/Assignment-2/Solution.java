d04d8ef0ccce1b69c22c71990b1febe5a75596c9
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-31 13:55:29 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Solution {
+	/* Fill the main function to print resultant of addition of matrices*/
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] a = {};
+		int[][] b = {};
+		int[][] c = new int[3][3];
+		for (int i=0;i<3;i++) {
+			for (int j=0;j<3;j++) {
+				c[i][j]=a[i][j]+b[i][j];
+				System.out.println(c[i][j]);
+			}
+		}
+
+		
+	}
+}
