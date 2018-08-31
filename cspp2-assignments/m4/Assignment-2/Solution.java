1d980eaec8f6e744c57e700d43d30db3d968d05d
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-31 18:39:09 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -18,7 +18,7 @@ public class Solution {
         int n1 = sc.nextInt();
         sc.nextLine();
         int[][] b = new int[m1][n1];
-        int[][] c = new int[3][3];
+        int[][] c = new int[m1][n1];
         if(m1==3 && n1==3) {
             for (int k = 0; k < m1; k++) {
                 for (int l = 0; l < n1; l++) {
