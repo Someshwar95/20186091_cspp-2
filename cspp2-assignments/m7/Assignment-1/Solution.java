a8faa9726eecd8491a993fb9700e2ac04914432a
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-09-03 21:28:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -15,10 +15,8 @@ class InputValidator
         }
     }
     }
-public class Solution
-{
-    public static void main(String args[])
-    {
+public class Solution {
+    public static void main(final String args[]) {
         Scanner s = new Scanner(System.in);
         String input = s.next();
         InputValidator i = new InputValidator(input);
