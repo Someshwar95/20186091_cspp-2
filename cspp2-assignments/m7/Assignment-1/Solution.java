66402292429047069a051b169e6c026b5be1e3cc
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-09-03 20:22:08 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -1,28 +1,28 @@
 import java.util.*;
 class InputValidator
 {
-	String str; 
-	public InputValidator(String s) {
-	this.str = s;
-	}
+    String str; 
+    public InputValidator(String s) {
+    this.str = s;
+    }
 
-	/*Write the atrributes and methods for InputValidator*/
-	public boolean validateData() {
-		if(str.length()>=6) {
-			return true;
-		} else{
-			return false;
-		}
-	}
-	}
+    /*Write the atrributes and methods for InputValidator*/
+    public boolean validateData() {
+        if(str.length()>=6) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+    }
 public class Solution
 {
-	public static void main(String args[])
+    public static void main(String args[])
     {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
     }
 
