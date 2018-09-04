9aab63abfd85ccbba0413738842b12a19e243e3a
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-09-04 18:58:08 +0530

    "check_style_score -> 1 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m8/CodecampList/List.java
+++ cspp2-assignments/m8/CodecampList/List.java
@@ -59,6 +59,9 @@ public class List {
     // declare a private int size
     // again, don't initialize it here
     // variable initialization should be done in the constructor
+    /**.
+    private
+    */
     private int size;
 
     /*
