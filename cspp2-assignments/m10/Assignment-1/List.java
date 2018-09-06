76d30129e48fb1a4252cd39f40486cc9d960cd32
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-09-06 11:30:24 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -233,7 +233,7 @@ public class List {
      */
     public String toString() {
         if (size == 0) {
-            return "";
+            return "[]";
         }
         String str = "[";
         int i = 0;
