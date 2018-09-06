8f37a555b74f50bbf3671795cbf7593ded232c92
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-09-06 12:28:44 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -412,7 +412,7 @@ public class List {
                 case "addAll":
                 if (tokens.length == 2) {
                 String[] t1 = tokens[1].split(",");
-                int temp[] = new int[t1.length];
+                int[] temp = new int[t1.length];
                 for (int i = 0; i < temp.length; i++) {
                     temp[i] = Integer.parseInt(t1[i]);
                 }
@@ -447,3 +447,7 @@ public class List {
         }
     }
 }
+
+
+
+
