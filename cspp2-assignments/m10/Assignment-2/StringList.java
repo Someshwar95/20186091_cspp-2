0cc0bfecc54768f5eb2c11c3bc24ec04525f292b
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-09-06 18:19:11 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/StringList.java
+++ cspp2-assignments/m10/Assignment-2/StringList.java
@@ -119,6 +119,7 @@ public class StringList implements StringListInterface{
      * The method returns void (nothing)
      */
     public void add(String item) {
+        
 
         //Inserts the specified element at the end of the list.
 
