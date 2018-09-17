1039a13da4ea0a88e4be073ae43b3dacaff94e4b
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-09-17 11:28:14 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m20/Assignment-1/Solution.java
+++ cspp2-assignments/m20/Assignment-1/Solution.java
@@ -235,7 +235,6 @@ public final class Solution {
     public static void loadQuestions(final Scanner scan,
         final Quiz quiz, final int q) {
     	System.out.println("4 are added to the quiz");
-    	Scanner sc = new Scanner(System.in);
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
