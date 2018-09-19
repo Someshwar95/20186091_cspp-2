33c84170c3bd40c807a1283ae2d2796a2435d9a7
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-09-19 10:00:40 +0530

    "check_style_score -> 13 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m20/Assignment-1/Solution.java
+++ cspp2-assignments/m20/Assignment-1/Solution.java
@@ -258,7 +258,9 @@ public final class Solution {
                 System.out.println("|--------------|");
                 System.out.println("| Score Report |");
                 System.out.println("|--------------|");
-                displayScore(q);
+                if (flag) {
+                    displayScore(q);
+                }
                 break;
                 default:
                 break;
